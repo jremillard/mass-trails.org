bfba3c9160049ae2d9c75725336c107105a0033f
Jason Remillard <remillard.jason@gmail.com>
2020-11-28 21:24:16 -0500

    automate more of the deployment

--- src/downloadmakesite.py
+++ src/downloadmakesite.py
@@ -13,4 +13,15 @@ os.system('python getosm.py')
 os.system("python properties.py")
 os.system("python makepages.py")
 os.system("python trails.py")
+os.system("python sitemap.py")
+os.system("browserify search.js --outfile ../scripts/search.js")
+os.system("pushmasstrails.py")
+
+
+# MAPBOX_ACCESS_TOKEN= to upload token env
+
+#os.system("mapbox upload mass-trails.7otdywgx --name ma-trails-osm-cqxncf trails-osm.geojson")
+os.system("mapbox upload mass-trails.01ptq61x --name ma-properties-osm-0y6yww properties-osm.geojson")
+
+
 
---
+++ src/pushmasstrails.py
@@ -0,0 +1,25 @@
+import os
+
+# goto shared access signature, use search function, should be like this, starting with ?sv= "?sv=2019-12-12&ss= bla bla bla"
+
+key = os.environ['MASSTRAILS-AZURE-SAS']
+
+updateSat = False
+updateTiles = True
+
+
+if ( updateTiles):
+    cmd = "azcopy rm \"https://masstrails.blob.core.windows.net/$web/*" + key + "\" --recursive"
+    #print(cmd)
+    #os.system(cmd)
+
+    cmd = "azcopy copy ..\* \"https://masstrails.blob.core.windows.net/$web" + key + "\" --recursive --exclude-path=\"src;mapstyles\" --exclude-pattern=\"*.py;*.exe;.gitignore;.htaccess;.git\""
+    print(cmd)
+    os.system(cmd)
+
+
+
+
+
+
+
